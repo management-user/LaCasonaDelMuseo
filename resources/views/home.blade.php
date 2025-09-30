1d988ccaafe4ff8efa358c0629f7e417cace3073
management-user <josephprada.fa@academia.umb.edu.co>
2025-09-30 15:50:24 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,42 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Página Principal</title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <title>La Casona del Museo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="/resources/css/styles.css">
 </head>
 <body>
-    <h1>🎉 ¡Bienvenido a Mi Casona Web! 🎉</h1>
-    <p>¡Tu página principal de Laravel ya está funcionando correctamente!</p>
+    <header>
+        <nav class="navigation-bar">
+            <a class="logo-link" href="/index.html">
+                <img src="/resources/images/logo.jpg" alt="Logo" class="main-logo">
+                <p class="Logo-text">La Casona Del Museo</p>
+            </a>
+
+            <div class="spacer"></div>
+
+            <ul class="principal-menu">
+                <li><a href="#">Contacto</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Content -->
+    <main>
+        <nav class="navigation-links">
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Nuestra Historia</a></li>
+                <li><a href="#">Productos</a></li>
+            </ul>
+            
+        </nav>
+        
+    </main>
+
+    <footer></footer>
 </body>
 </html>
+
+<!--https://es.squarespace.com/diseno-web?gclsrc=aw.ds&gad_source=1&gbraid=0AAAAADxS_FKM5luoh1IhA4WWtyScq2OCg-->
