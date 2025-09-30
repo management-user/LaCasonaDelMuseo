fbfcd88a099f2a1cd804d5f7fb762c4add1d19ab
Joseph <josephprada.fa@academia.umb.edu.co>
2025-09-30 00:46:22 -0500

    Initial commit de proyecto Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Página Principal</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+    <h1>🎉 ¡Bienvenido a Mi Casona Web! 🎉</h1>
+    <p>¡Tu página principal de Laravel ya está funcionando correctamente!</p>
+</body>
+</html>
